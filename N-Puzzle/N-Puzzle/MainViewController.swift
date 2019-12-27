70f23da00b710340e9a46e74ba23c14714309362
Denys Tomchyshen <denys.tomchyshen@codeit.com.ua>
2019-12-27 17:44:55 +0200

    FisrtProject commit

---
+++ N-Puzzle/N-Puzzle/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  N-Puzzle
+//
+//  Created by Denys Tomchyshen on 27.12.2019.
+//  Copyright © 2019 Ddryha&Dtomchys. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+
+    @IBAction func main(_ sender: Any) {
+
+    }
+
+}
